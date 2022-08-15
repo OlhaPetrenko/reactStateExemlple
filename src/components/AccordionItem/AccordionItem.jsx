94cf6470433ca626e5c5c6b21345931ae4b71d40
OlhaPetrenko <o.l.petr27@gmail.com>
2022-08-15 14:25:30 +0300

    Add ex. 1

---
+++ src/components/AccordionItem/AccordionItem.jsx
@@ -0,0 +1,36 @@
+import { Component } from 'react';
+
+class AccordionItem extends Component {
+  state = {
+    isShow: false,
+  };
+  clickBtn = () => {
+    this.setState({
+      isShow: !this.state.isShow,
+    });
+  };
+
+  render() {
+    const { clickBtn } = this;
+    return (
+      <div>
+        <button type="button" onClick={clickBtn}>
+          {' '}
+          Click me!
+        </button>
+
+        {this.state.isShow && (
+          <div className={s.}>
+            {' '}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ut
+            distinctio porro beatae ea delectus earum aliquam fugit asperiores.
+            Labore, dicta vel. Autem soluta cumque saepe perferendis
+            perspiciatis officiis quos.
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default AccordionItem;
