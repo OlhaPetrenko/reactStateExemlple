fbf8d12e06a4a07b9b3ca8d20f41045ea0982186
OlhaPetrenko <o.l.petr27@gmail.com>
2022-08-19 15:54:01 +0300

    Add Form Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,34 @@
-import AccordionItem from './AccordionItem/AccordionItem';
-import List from './List/List';
+// import AccordionItem from './AccordionItem/AccordionItem';
+// import List from './List/List';
+import ProductForm from './ProductForm/ProductForm';
+import { Component } from 'react';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <AccordionItem />
-      <List />
-    </div>
-  );
-};
+// export const App = () => {
+
+//   return (
+//     <div>
+//       <ProductForm />
+//       {/* <AccordionItem /> */}
+//       {/* <List /> */}
+//     </div>
+//   );
+// };
+
+class App extends Component {
+  state = {
+    products: [],
+  };
+
+  onSubmit = product => {
+    this.setState(prevState => {
+      return {
+        products: [...prevState.products, product],
+      };
+    });
+  };
+  render() {
+    return <ProductForm onSubmit={this.onSubmit} />;
+  }
+}
+
+export default App;
